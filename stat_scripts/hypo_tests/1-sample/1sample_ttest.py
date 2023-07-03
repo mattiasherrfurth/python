50fff7770d19a11c03622f9da0cd4f0d084f3473
Herrfurth <J20032@NGC.AUTHORITY>
2023-07-03 10:36:04 -0400

    condensed 1sample functions to two-sided function calling greater-than and less-than

--- stat_scripts/hypo_tests/1-sample/1sample_ttest.py
+++ stat_scripts/hypo_tests/1-sample/1sample_ttest.py
@@ -13,47 +13,43 @@ target = 200
 ############### FUNCTIONS ###############
 
 # INPUT: a dataframe, the numeric column, and the target being tested
-# OUTPUT: the results of the 1-sample t-test
+# OUTPUT: the results of the 1-sample t-test, testing two-sided, greater-than, and less-than
 # ASSUMPTIONS: the confidence level is 95% (alpha = 0.05)
-def OneSampleTtest_twosided(df,col,target):
+def OneSampleTtest(df,col,target):
     t_stat, p_val = stats.ttest_1samp(a=df[col],popmean=target)
     print('These are the descriptive statistics for the sample: \n%s\n'%df[col].describe())
     print('The t-statistic is %s'%t_stat)
-    print('The p-value is %s'%p_val)
+    print('The p-value for two-sided testing is %s'%p_val)
     if p_val < 0.05:
-        print('Reject null hypothesis. The sample is statistically different than the target.\n')
+        print('Reject null hypothesis for two-sided t-test. The sample mean is statistically different than the target.\n')
+        OneSampleTtest_greater(df,col,target)
+        OneSampleTtest_less(df,col,target)
     else:
-        print('Accept null hypothesis. The sample is not statistically different than the target.\n')
+        print('Accept null hypothesis for two-sided t-test. The sample mean is not statistically different than the target.\n')
 
 # INPUT: a dataframe, the numeric column, and the target being tested
 # OUTPUT: the results of the 1-sample t-test testing for whether the sample mean is greater than the target
 # ASSUMPTIONS: the confidence level is 95% (alpha = 0.05)
 def OneSampleTtest_greater(df,col,target):
     t_stat, p_val = stats.ttest_1samp(a=df[col],popmean=target,alternative='greater')
-    print('These are the descriptive statistics for the sample: \n%s\n'%df[col].describe())
-    print('The t-statistic is %s'%t_stat)
-    print('The p-value is %s'%p_val)
+    print('The p-value when testing for greater is %s'%p_val)
     if p_val < 0.05:
-        print('Reject null hypothesis. The sample is statistically greater than the target.\n')
+        print('Reject null hypothesis for greater than t-test. The sample mean is statistically greater than the target.\n')
     else:
-        print('Accept null hypothesis. The sample is not statistically greater than the target.\n')
+        print('Accept null hypothesis for greater than t-test. The sample mean is not statistically greater than the target.\n')
 
 # INPUT: a dataframe, the numeric column, and the target being tested
-# OUTPUT: the results of the 1-sample t-test testing for whether the sample mean is greater than the target
+# OUTPUT: the results of the 1-sample t-test testing for whether the sample mean is less than the target
 # ASSUMPTIONS: the confidence level is 95% (alpha = 0.05)
 def OneSampleTtest_less(df,col,target):
     t_stat, p_val = stats.ttest_1samp(a=df[col],popmean=target,alternative='less')
-    print('These are the descriptive statistics for the sample: \n%s\n'%df[col].describe())
-    print('The t-statistic is %s'%t_stat)
-    print('The p-value is %s'%p_val)
+    print('The p-value when testing for less is %s'%p_val)
     if p_val < 0.05:
-        print('Reject null hypothesis. The sample is statistically less than the target.\n')
+        print('Reject null hypothesis for less than t-test. The sample mean is statistically less than the target.\n')
     else:
-        print('Accept null hypothesis. The sample is not statistically less than the target.\n')
+        print('Accept null hypothesis for less than t-test. The sample mean is not statistically less than the target.\n')
 
 ############### MAIN ###############
 
 if __name__ == "__main__":
-    OneSampleTtest_twosided(df,col,target)
-    OneSampleTtest_greater(df,col,target)
-    OneSampleTtest_less(df,col,target)
+    OneSampleTtest(df,col,target)
--- stat_scripts/hypo_tests/1-sample/1sample_ztest.py
+++ stat_scripts/hypo_tests/1-sample/1sample_ztest.py
@@ -13,47 +13,43 @@ target = 15
 ############### FUNCTIONS ###############
 
 # INPUT: a dataframe, the numeric column, and the target being tested
-# OUTPUT: the results of the 1-sample z-test
+# OUTPUT: the results of the 1-sample z-test, testing two-sided, greater-than, and less-than
 # ASSUMPTIONS: the confidence level is 95% (alpha = 0.05)
-def OneSampleZtest_twosided(df,col,target):
+def OneSampleZtest(df,col,target):
     z_stat, p_val = ztest(df[col],value=target)
     print('These are the descriptive statistics for the sample: \n%s\n'%df[col].describe())
     print('The z-statistic is %s'%z_stat)
-    print('The p-value is %s'%p_val)
+    print('The p-value for two-sided testing is %s'%p_val)
     if p_val < 0.05:
-        print('Reject null hypothesis. The sample is statistically different than the target.\n')
+        print('Reject null hypothesis for two-sided z-test. The sample mean is statistically different than the target.\n')
+        OneSampleZtest_larger(df,col,target)
+        OneSampleZtest_smaller(df,col,target)
     else:
-        print('Accept null hypothesis. The sample is not statistically different than the target.\n')
+        print('Accept null hypothesis for two-sided z-test. The sample mean is not statistically different than the target.\n')
 
 # INPUT: a dataframe, the numeric column, and the target being tested
 # OUTPUT: the results of the 1-sample z-test testing for whether the sample mean is greater than the target
 # ASSUMPTIONS: the confidence level is 95% (alpha = 0.05)
 def OneSampleZtest_larger(df,col,target):
     z_stat, p_val = ztest(df[col],value=target,alternative='larger')
-    print('These are the descriptive statistics for the sample: \n%s\n'%df[col].describe())
-    print('The z-statistic is %s'%z_stat)
-    print('The p-value is %s'%p_val)
+    print('The p-value when testing for greater is %s'%p_val)
     if p_val < 0.05:
-        print('Reject null hypothesis. The sample is statistically greater than the target.\n')
+        print('Reject null hypothesis for greater than z-test. The sample mean is statistically greater than the target.\n')
     else:
-        print('Accept null hypothesis. The sample is not statistically greater than the target.\n')
+        print('Accept null hypothesis for greater than z-test. The sample mean is not statistically greater than the target.\n')
 
 # INPUT: a dataframe, the numeric column, and the target being tested
-# OUTPUT: the results of the 1-sample z-test testing for whether the sample mean is greater than the target
+# OUTPUT: the results of the 1-sample z-test testing for whether the sample mean is less than the target
 # ASSUMPTIONS: the confidence level is 95% (alpha = 0.05)
 def OneSampleZtest_smaller(df,col,target):
     z_stat, p_val = ztest(df[col],value=target,alternative='smaller')
-    print('These are the descriptive statistics for the sample: \n%s\n'%df[col].describe())
-    print('The z-statistic is %s'%z_stat)
-    print('The p-value is %s'%p_val)
+    print('The p-value when testing for less is %s'%p_val)
     if p_val < 0.05:
-        print('Reject null hypothesis. The sample is statistically less than the target.\n')
+        print('Reject null hypothesis for less than z-test. The sample mean is statistically less than the target.\n')
     else:
-        print('Accept null hypothesis. The sample is not statistically less than the target.\n')
+        print('Accept null hypothesis for less than z-test. The sample mean is not statistically less than the target.\n')
 
 ############### MAIN ###############
 
 if __name__ == "__main__":
-    OneSampleZtest_twosided(df,col,target)
-    OneSampleZtest_larger(df,col,target)
-    OneSampleZtest_smaller(df,col,target)
+    OneSampleZtest(df,col,target)
